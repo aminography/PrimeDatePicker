6dc96763a0dbf7a12925755bd852e9442c87d0af
aminography <amin.maxim@gmail.com>
2019-05-26 18:51:05 +0430

    Adding CalendarView

--- sample-app/src/main/java/com/aminography/primedatepicker/sample/view/CalendarActivity.kt
+++ sample-app/src/main/java/com/aminography/primedatepicker/sample/view/CalendarActivity.kt
@@ -3,11 +3,17 @@ package com.aminography.primedatepicker.sample.view
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.aminography.primedatepicker.sample.R
+import kotlinx.android.synthetic.main.activity_calendar.*
 
 class CalendarActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calendar)
+
+        gotoButton.text = "Goto: 1991/6"
+        gotoButton.setOnClickListener {
+            calendarView.goto(1991, 6)
+        }
     }
 }
