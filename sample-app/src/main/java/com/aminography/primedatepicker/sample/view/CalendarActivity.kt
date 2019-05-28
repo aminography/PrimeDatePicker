610ab01fdb42a4e986f64194d47cd3f4a347bc64
aminography <amin.maxim@gmail.com>
2019-05-28 17:12:04 +0430

    Improving range selection

--- sample-app/src/main/java/com/aminography/primedatepicker/sample/view/CalendarActivity.kt
+++ sample-app/src/main/java/com/aminography/primedatepicker/sample/view/CalendarActivity.kt
@@ -3,7 +3,6 @@ package com.aminography.primedatepicker.sample.view
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.aminography.primedatepicker.PickType
-import com.aminography.primedatepicker.monthview.PrimeMonthView
 import com.aminography.primedatepicker.sample.R
 import com.aminography.primedatepicker.tools.Utils
 import kotlinx.android.synthetic.main.activity_calendar.*
@@ -17,17 +16,18 @@ class CalendarActivity : AppCompatActivity() {
 
         nowButton.setOnClickListener {
             val calendar = Utils.newCalendar()
-//            calendarView.goto(calendar.year, 1, true)
             calendarView.goto(calendar.year, calendar.month, true)
         }
 
         gotoPastButton.setOnClickListener {
-            calendarView.goto(2019, 1, true)
-//            calendarView.goto(2014, 9, true)
+            val calendar = Utils.newCalendar()
+            calendar.month = 1
+            calendarView.goto(calendar.year, calendar.month, true)
         }
         gotoFutureButton.setOnClickListener {
-            calendarView.goto(2019, 10, true)
-//            calendarView.goto(2022, 1, true)
+            val calendar = Utils.newCalendar()
+            calendar.month = 11
+            calendarView.goto(calendar.year, calendar.month, true)
         }
 
         setMinDateButton.setOnClickListener {
