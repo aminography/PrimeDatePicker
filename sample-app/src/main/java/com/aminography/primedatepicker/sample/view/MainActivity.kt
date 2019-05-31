652b0cf732b7fd921476113d34c446552ad8702f
aminography <amin.maxim@gmail.com>
2019-05-31 21:14:30 +0430

    Adding stand alone CalendarView and MonthView showcases

--- sample-app/src/main/java/com/aminography/primedatepicker/sample/view/MainActivity.kt
+++ sample-app/src/main/java/com/aminography/primedatepicker/sample/view/MainActivity.kt
@@ -73,7 +73,7 @@ class MainActivity : AppCompatActivity() {
         }
 
 //        calendarViewButton.performClick()
-        monthViewButton.performClick()
+//        monthViewButton.performClick()
     }
 
 }
