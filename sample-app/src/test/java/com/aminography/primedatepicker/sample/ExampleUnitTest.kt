cd2405fd75653686a816b7279c0f0e663675779a
aminography <amin.maxim@gmail.com>
2019-06-17 12:27:14 +0430

    Updating PrimeCalendar library

--- sample-app/src/test/java/com/aminography/primedatepicker/sample/ExampleUnitTest.kt
+++ sample-app/src/test/java/com/aminography/primedatepicker/sample/ExampleUnitTest.kt
@@ -1,7 +1,6 @@
 package com.aminography.primedatepicker.sample
 
-import com.aminography.primecalendar.persian.PersianCalendar
-import org.junit.Assert.assertEquals
+import junit.framework.Assert.assertEquals
 import org.junit.Test
 
 /**
@@ -12,31 +11,8 @@ import org.junit.Test
 class ExampleUnitTest {
 
     @Test
-    fun persianToCivilConversion() {
-        val persian = PersianCalendar()
-        persian.setDate(1370, 3, 15)
-        println("Persian Date: ${persian.longDateString}")
-
-        val civil = persian.toCivil()
-        println("Civil Date: ${civil.longDateString}")
-
-        assertEquals(civil.year, 1991)
-        assertEquals(civil.month, 6)
-        assertEquals(civil.dayOfMonth, 6)
-    }
-
-    @Test
-    fun persianToHijriConversion() {
-        val persian = PersianCalendar()
-        persian.setDate(1370, 3, 15)
-        println("Persian Date: ${persian.longDateString}")
-
-        val hijri = persian.toHijri()
-        println("Hijri Date: ${hijri.longDateString}")
-
-        assertEquals(hijri.year, 1411)
-        assertEquals(hijri.month, 11)
-        assertEquals(hijri.dayOfMonth, 23)
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
     }
 
 }
