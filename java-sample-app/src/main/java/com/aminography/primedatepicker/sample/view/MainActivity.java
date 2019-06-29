76be181adddf8953fb3d9aec291b837a5fbfd5e1
aminography <amin.maxim@gmail.com>
2019-06-29 19:13:23 +0430

    Updating readme

--- java-sample-app/src/main/java/com/aminography/primedatepicker/sample/view/MainActivity.java
+++ java-sample-app/src/main/java/com/aminography/primedatepicker/sample/view/MainActivity.java
@@ -1,17 +1,8 @@
 package com.aminography.primedatepicker.sample.view;
 
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.annotation.Nullable;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.AppCompatCheckBox;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.CompoundButton;
 
 import com.aminography.primedatepicker.sample.R;
 
