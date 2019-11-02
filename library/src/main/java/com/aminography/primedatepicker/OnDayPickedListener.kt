703b79acfcc6cc53ba20221455aaf64ca19cb922
Amin <amin.maxim@gmail.com>
2019-11-02 21:53:15 +0330

    Some refactoring.

--- library/src/main/java/com/aminography/primedatepicker/OnDayPickedListener.kt
+++ library/src/main/java/com/aminography/primedatepicker/OnDayPickedListener.kt
@@ -7,6 +7,19 @@ import com.aminography.primecalendar.PrimeCalendar
  */
 interface OnDayPickedListener {
 
-    fun onDayPicked(pickType: PickType, singleDay: PrimeCalendar?, startDay: PrimeCalendar?, endDay: PrimeCalendar?)
+    /**
+     * Called when one day is clicked by the user.
+     *
+     * @param pickType Specifies type of the picked day based on [PickType] which is set to the [com.aminography.primedatepicker.monthview.PrimeMonthView] or [com.aminography.primedatepicker.calendarview.PrimeCalendarView].
+     * @param singleDay Specifies the picked day if the [pickType] is [PickType.SINGLE].
+     * @param startDay Specifies the start day of picked range if the [pickType] is [PickType.RANGE_START].
+     * @param endDay Specifies the end day of picked range if the [pickType] is [PickType.RANGE_END].
+     */
+    fun onDayPicked(
+            pickType: PickType,
+            singleDay: PrimeCalendar?,
+            startDay: PrimeCalendar?,
+            endDay: PrimeCalendar?
+    )
 
 }
--- library/src/main/java/com/aminography/primedatepicker/tools/ExtensionFunctions.kt
+++ library/src/main/java/com/aminography/primedatepicker/tools/ExtensionFunctions.kt
@@ -11,11 +11,26 @@ import com.aminography.primecalendar.PrimeCalendar
  * @author aminography
  */
 
+/**
+ * Calculates month offset of the current calendar.
+ *
+ * @return month offset of the current calendar.
+ */
 fun PrimeCalendar.monthOffset(): Int =
         year * 12 + month
 
+/**
+ * Checks the orientation of the screen.
+ *
+ * @return true if the orientation of the screen is landscape, false if the orientation is portrait.
+ */
 fun Context.isDisplayLandscape(): Boolean = (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE)
 
+/**
+ * Returns screen size of the device in pixels.
+ *
+ * @return screen size of the device as a [Point] object.
+ */
 fun Context.screenSize(): Point {
     val windowManager = getSystemService(Context.WINDOW_SERVICE) as WindowManager
     val display: Display? = windowManager.defaultDisplay
