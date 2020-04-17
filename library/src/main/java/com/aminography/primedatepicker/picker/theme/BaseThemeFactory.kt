ccb66f7c627c4ad472d7ba311b084647d3c89e9a
Amin <amin.hassani@cafebazaar.cloud>
2020-04-17 16:10:34 +0430

    Creating base skeleton of theme feature.

---
+++ library/src/main/java/com/aminography/primedatepicker/picker/theme/BaseThemeFactory.kt
@@ -0,0 +1,67 @@
+package com.aminography.primedatepicker.picker.theme
+
+import android.view.animation.Interpolator
+import com.aminography.primedatepicker.calendarview.PrimeCalendarView
+
+/**
+ * @author aminography
+ */
+abstract class BaseThemeFactory {
+
+    abstract val monthLabelTextColor: Int
+
+    abstract val weekLabelTextColor: Int
+
+    abstract val dayLabelTextColor: Int
+
+    abstract val todayLabelTextColor: Int
+
+    abstract val pickedDayLabelTextColor: Int
+
+    abstract val pickedDayCircleColor: Int
+
+    abstract val disabledDayLabelTextColor: Int
+
+    abstract val monthLabelTextSize: Int
+
+    abstract val weekLabelTextSize: Int
+
+    abstract val dayLabelTextSize: Int
+
+    abstract val monthLabelTopPadding: Int
+
+    abstract val monthLabelBottomPadding: Int
+
+    abstract val weekLabelTopPadding: Int
+
+    abstract val weekLabelBottomPadding: Int
+
+    abstract val dayLabelVerticalPadding: Int
+
+    abstract val showTwoWeeksInLandscape: Boolean
+
+    abstract val animationDuration: Int
+
+    abstract val animationInterpolator: Interpolator
+
+    abstract val loadFactor: Int
+
+    abstract val maxTransitionLength: Int
+
+    abstract val transitionSpeedFactor: Float
+
+    abstract val dividerColor: Int
+
+    abstract val dividerThickness: Int
+
+    abstract val dividerInsetLeft: Int
+
+    abstract val dividerInsetRight: Int
+
+    abstract val dividerInsetTop: Int
+
+    abstract val dividerInsetBottom: Int
+
+    abstract val flingOrientation: PrimeCalendarView.FlingOrientation
+
+}
---
+++ library/src/main/java/com/aminography/primedatepicker/picker/theme/LightThemeFactory.kt
@@ -0,0 +1,95 @@
+package com.aminography.primedatepicker.picker.theme
+
+import android.view.animation.Interpolator
+import com.aminography.primedatepicker.calendarview.PrimeCalendarView
+
+/**
+ * @author aminography
+ */
+class LightThemeFactory : BaseThemeFactory() {
+
+    override val monthLabelTextColor: Int
+        get() = TODO("Not yet implemented")
+
+    override val weekLabelTextColor: Int
+        get() = TODO("Not yet implemented")
+
+    override val dayLabelTextColor: Int
+        get() = TODO("Not yet implemented")
+
+    override val todayLabelTextColor: Int
+        get() = TODO("Not yet implemented")
+
+    override val pickedDayLabelTextColor: Int
+        get() = TODO("Not yet implemented")
+
+    override val pickedDayCircleColor: Int
+        get() = TODO("Not yet implemented")
+
+    override val disabledDayLabelTextColor: Int
+        get() = TODO("Not yet implemented")
+
+    override val monthLabelTextSize: Int
+        get() = TODO("Not yet implemented")
+
+    override val weekLabelTextSize: Int
+        get() = TODO("Not yet implemented")
+
+    override val dayLabelTextSize: Int
+        get() = TODO("Not yet implemented")
+
+    override val monthLabelTopPadding: Int
+        get() = TODO("Not yet implemented")
+
+    override val monthLabelBottomPadding: Int
+        get() = TODO("Not yet implemented")
+
+    override val weekLabelTopPadding: Int
+        get() = TODO("Not yet implemented")
+
+    override val weekLabelBottomPadding: Int
+        get() = TODO("Not yet implemented")
+
+    override val dayLabelVerticalPadding: Int
+        get() = TODO("Not yet implemented")
+
+    override val showTwoWeeksInLandscape: Boolean
+        get() = TODO("Not yet implemented")
+
+    override val animationDuration: Int
+        get() = TODO("Not yet implemented")
+
+    override val animationInterpolator: Interpolator
+        get() = TODO("Not yet implemented")
+
+    override val loadFactor: Int
+        get() = TODO("Not yet implemented")
+
+    override val maxTransitionLength: Int
+        get() = TODO("Not yet implemented")
+
+    override val transitionSpeedFactor: Float
+        get() = TODO("Not yet implemented")
+
+    override val dividerColor: Int
+        get() = TODO("Not yet implemented")
+
+    override val dividerThickness: Int
+        get() = TODO("Not yet implemented")
+
+    override val dividerInsetLeft: Int
+        get() = TODO("Not yet implemented")
+
+    override val dividerInsetRight: Int
+        get() = TODO("Not yet implemented")
+
+    override val dividerInsetTop: Int
+        get() = TODO("Not yet implemented")
+
+    override val dividerInsetBottom: Int
+        get() = TODO("Not yet implemented")
+
+    override val flingOrientation: PrimeCalendarView.FlingOrientation
+        get() = TODO("Not yet implemented")
+
+}
